557083be1db501f8c4883d6aeb135df1e6322b66
schitoroaga <56911400+schitoroaga@users.noreply.github.com>
2019-12-22 19:56:50 +0200

    Add files via upload

---
+++ SimonBrad.java
@@ -0,0 +1,33 @@
+
+public class SimonBrad {
+ public static void main(String[] args) {
+	
+	 char a='*';
+	 char b='^';
+	 System.out.printf("%8C",b,b);
+	 System.out.printf("%n%6C%2C%2C",b,b,b);
+	 System.out.printf("%n%8C",b,b);
+	 System.out.printf("%n%8C",a,a);
+	 System.out.printf("%n%6C%2C%2C",a,b,a);
+	 System.out.printf("%n%4C%2C%2C%2C%2C",a,b,a,b,a);
+     System.out.printf("%n%2C%2C%2C%2C%2C%2C%2C",b,a,a,b,a,a,b);
+	 System.out.printf("%n%C%2C%2C%2C%2C%2C%2C%2C",a,b,a,a,a,a,b,a);
+	 System.out.printf("%n%6C%2C%2C",a,a,a);
+	 System.out.printf("%n%6C%2C%2C",a,a,a);
+	 System.out.printf("%n%6C%2C%2C",a,a,a);
+	 System.out.printf("%nMerry Christmas");
+	 
+	 
+	 
+ }
+ 
+
+
+
+
+
+
+
+
+
+}
