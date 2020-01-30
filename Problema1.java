8bd92d8e75fcd2dfb4bf695911649fe0f111fb97
schitoroaga <56911400+schitoroaga@users.noreply.github.com>
2020-01-30 19:41:21 +0200

    Java IF Statement

---
+++ Problema1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Problema1 {
+	public static void main(String args[]) {
+		int a,b,i;
+		Scanner sc=new Scanner(System.in);
+		a= sc.nextInt();
+		b= sc.nextInt();
+		if (a>0) i=a*a;
+		else i=a+b;
+		System.out.println(i);
+	
+		
+	}
+
+}
